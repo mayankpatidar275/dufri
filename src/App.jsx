8bc86b8d8ecf37a6f217da4b5dbccb46b5ed851d
thinkmetal-mayank <mayank@thinkmetal.in>
2024-02-28 10:43:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() { 
+
+  return (
+    <div>
+      <button>Start server</button>
+    </div> 
+  );
+}
+
+export default App;
